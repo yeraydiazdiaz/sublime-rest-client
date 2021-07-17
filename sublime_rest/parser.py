14e5d1fabd9fb538818143a1e166ce5830457b30
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2021-07-17 12:00:33 +0100

    Add method to parser

--- sublime_rest/parser.py
+++ sublime_rest/parser.py
@@ -1,6 +1,20 @@
 from sublime_rest import Request
 
+BOUNDARY = "###"
 
-def parse(contents, pos):
-    row, col = pos
-    return Request("https://example.com")
+
+def parse(contents: str, pos: int) -> Request:
+    block = _get_request_block(contents, pos)
+    method = "GET"
+    url = block
+    if " " in url:
+        method, url = block.split()
+    return Request(url=url, method=method)
+
+
+def _get_request_block(contents: str, pos: int) -> str:
+    top = contents.find(BOUNDARY, 0, pos)
+    bottom = contents.find(BOUNDARY, pos)
+    start = top if top != -1 else 0
+    end = bottom if bottom != -1 else None
+    return contents[start:end].strip()
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,10 +1,19 @@
+import pytest
+
 from sublime_rest import Request, parser
 
 
-def test_single_line():
+def test_single_line_no_method():
     contents = "https://example.com"
-    pos = (0, 0)
 
-    req = parser.parse(contents, pos)
+    req = parser.parse(contents, 0)
 
     assert req == Request(url="https://example.com")
+
+
+def test_single_line_method():
+    contents = "POST https://example.org"
+
+    req = parser.parse(contents, 0)
+
+    assert req == Request(url="https://example.org", method="POST")
