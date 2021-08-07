5b9fb63cfa1e178480bca48912a63609819a6c95
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2021-08-07 18:05:36 +0100

    Wrap request in a thread

--- plugin.py
+++ plugin.py
@@ -2,6 +2,9 @@ import os.path
 import sys
 import typing as tp
 from http import HTTPStatus
+import threading
+import traceback
+from time import perf_counter
 
 # Import dependencies
 sys.path.append(os.path.dirname(__file__) + "/deps")
@@ -13,9 +16,32 @@ from .sublime_rest import client, parser
 from .sublime_rest.request import Request
 
 
+class HttpRequestThread(threading.Thread):
+    def __init__(self, request):
+        super().__init__()
+        self.request = request
+        self.success = None
+
+    def run(self):
+        self._start = perf_counter()
+        try:
+            self.result = client.request(self.request)
+            self.success = True
+        except Exception:
+            self.success = False
+            self.result = traceback.format_exc()
+        finally:
+            self._end = perf_counter()
+            self.elapsed = self._end - self._start
+
+    def get_result(self):
+        return self.result
+
+
 class RestRequestCommand(sublime_plugin.WindowCommand):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        self._tick = 0
 
     def run(self, *args) -> None:
         print("Running Sublime REST", args)
@@ -29,16 +55,40 @@ class RestRequestCommand(sublime_plugin.WindowCommand):
         self.log_to_status("Sending request for: `{}`".format(contents))
 
         request = parser.parse(contents, pos)
-        status, headers, body = client.request(request)
-        response_text = self.get_response_content(request, status, headers, body)
+
+        thread = HttpRequestThread(request)
+        thread.start()
+        self.handle_thread(thread)
+
+    def handle_thread(self, thread: HttpRequestThread) -> None:
+        if thread.is_alive():
+            dots = "".join("." if j != self._tick % 3 else " " for j in range(3))
+            self.log_to_status(f"Waiting for response {dots}")
+            self._tick += 1
+            sublime.set_timeout_async(lambda: self.handle_thread(thread), 100)
+        elif thread.success:
+            self.on_success(thread)
+        else:
+            self.on_error(thread)
+
+    def on_success(self, thread: HttpRequestThread) -> None:
+        msg = f"Response received in {thread.elapsed:.3f} seconds"
+        self.log_to_status(msg)
+        status, headers, body = thread.get_result()
+        response_text = self.get_response_content(thread.request, status, headers, body)
 
         response_view = self.window.new_file()
         response_view.run_command("rest_replace_view_text", {"text": response_text})
-        self.log_to_status(status)
+        self.log_to_status(msg, response_view)
 
-    def log_to_status(self, msg: str) -> None:
+    def on_error(self, thread: HttpRequestThread) -> None:
+        self.log_to_status("Unexpected error on request, please see logs")
+        print(thread.result)
+
+    def log_to_status(self, msg: str, view: sublime.View = None) -> None:
         """Displays the message in the status bar of the view."""
-        self.request_view.set_status("rest", "REST: {}".format(msg))
+        view = view or self.request_view
+        view.set_status("rest", "REST: {}".format(msg))
 
     def get_request_text_from_selection(self) -> str:
         """Expands the selection to the boundaries of the request."""
