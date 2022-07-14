a01d5083c2456943112c63ba1375e0683cc5a74e
Yeray Diaz <6739793+yeraydiazdiaz@users.noreply.github.com>
2022-07-14 13:35:03 +0100

    Encode payload in UTF-8
    urllib3 will default to encoding payloads as latin-1,
    when the payload has characters out of the range urllib3
    will raise an error. Encode the payload as utf-8 as suggested.

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -1,18 +1,24 @@
 import json
 
-from rest_client import Request, client
 from pytest_httpserver import HTTPServer
+
+from rest_client import Request, client
 
 
 def test_request(httpserver: HTTPServer) -> None:
     headers = {"Content-Type": "application/json"}
-    body = {"hello": "world"}
+    body = '{"hello": "world", "utf8": "is 🚀"}'
     response_body = {"hello": "back"}
-    httpserver.expect_request("/", headers=headers, json=body).respond_with_json(
+    httpserver.expect_request("/", headers=headers, data=body).respond_with_json(
         response_body
     )
 
-    req = Request(url=httpserver.url_for("/"), headers=headers, body=json.dumps(body))
+    req = Request(
+        method="POST",
+        url=httpserver.url_for("/"),
+        headers=headers,
+        body=body,
+    )
     response = client.request(req)
 
     assert response.status == 200
