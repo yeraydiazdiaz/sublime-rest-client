a211704730eea59571726e163a34e2e3061e353a
Yeray Diaz <6739793+yeraydiazdiaz@users.noreply.github.com>
2025-05-04 12:28:26 +0100

    Proxy support
    Support proxies configured via HTTP_PROXY and HTTPS_PROXY envionrment
    variable and `proxy_url` setting.

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -1,6 +1,9 @@
 import json
+import os
 
+import pytest
 from pytest_httpserver import HTTPServer
+import urllib3
 
 from rest_client import Request, client
 
@@ -19,6 +22,7 @@ def test_request(httpserver: HTTPServer) -> None:
         headers=headers,
         body=body,
     )
+    client.manager.configure({})
     response = client.request(req)
 
     assert response.status == 200
@@ -28,6 +32,20 @@ def test_request(httpserver: HTTPServer) -> None:
 def test_manager_configure_with_no_hosts() -> None:
     client.manager.configure({})
     assert client.manager.pools_per_host == {}
+
+
+@pytest.mark.parametrize("env_var_or_setting", ("ENV_VAR", "SETTING", None))
+def test_manager_configure_with_proxy(env_var_or_setting: str) -> None:
+    if env_var_or_setting == "ENV_VAR":
+        os.environ["HTTP_PROXY"] = "http://localhost:3128"
+        client.manager.configure({})
+        assert isinstance(client.manager.default_pool, urllib3.ProxyManager)
+    elif env_var_or_setting == "SETTING":
+        client.manager.configure({"proxy_url": "http://localhost:3128"})
+        assert isinstance(client.manager.default_pool, urllib3.ProxyManager)
+    else:
+        client.manager.configure({"proxy_url": "http://localhost:3128"})
+        assert isinstance(client.manager.default_pool, urllib3.PoolManager)
 
 
 def test_manager_configure_creates_connection_pools() -> None:
