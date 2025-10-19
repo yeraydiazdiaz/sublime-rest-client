3bc31af39895f882ee043d7cec084c7bc2565b3a
James Black <contact@jamesblack.ca>
2025-10-18 23:28:33 -0300

    Update integration tests to support the panel view

--- tests/integration/test_rest_client.py
+++ tests/integration/test_rest_client.py
@@ -9,6 +9,8 @@ from unittesting import DeferrableTestCase
 from . import utils
 
 TIMEOUT = int(os.getenv("REST_CLIENT_TIMEOUT", 500))
+PANEL_NAME = "REST Client Response"
+SETTINGS_FILE = "REST.sublime-settings"
 
 
 class TestRestClient(DeferrableTestCase):
@@ -16,13 +18,20 @@ class TestRestClient(DeferrableTestCase):
         self.window = sublime.active_window()
         self.view = self.window.new_file()
         self.response_view = None
-        settings = sublime.load_settings("Preferences.sublime-settings")
-        settings.set("close_windows_when_empty", False)
+        self.plugin_settings = sublime.load_settings(SETTINGS_FILE)
+        editor_settings = sublime.load_settings("Preferences.sublime-settings")
+        editor_settings.set("close_windows_when_empty", False)
 
     def tearDown(self) -> None:
+        # Make sure the UnitTesting panel is showing after the test run
+        self.window.run_command("show_panel", {"panel": "output.UnitTesting"})
+
         if self.response_view:
-            self.response_view.window().focus_view(self.response_view)
-            self.response_view.window().run_command("close_file")
+            if self.plugin_settings.get("response_view") == "panel":
+                self.window.destroy_output_panel(PANEL_NAME)
+            else:
+                self.response_view.window().focus_view(self.response_view)
+                self.response_view.window().run_command("close_file")
 
         if self.view:
             self.view.set_scratch(True)
@@ -30,7 +39,12 @@ class TestRestClient(DeferrableTestCase):
             self.view.window().run_command("close_file")
 
     def _get_response_view_contents(self) -> str:
-        self.response_view = self.window.active_view()
+        if self.plugin_settings.get("response_view") == "panel":
+            self.response_view = self.window.find_output_panel(PANEL_NAME)
+        else:
+            self.response_view = self.window.active_view()
+
+        self.assertIsNotNone(self.response_view, "REST response view not available")
         return utils.get_contents_of_view(self.response_view)
 
     def _get_response(
@@ -49,6 +63,7 @@ class TestRestClient(DeferrableTestCase):
         return status, headers, cast(str, body)
 
     def test_simple_get(self) -> Iterator[int]:
+        self.plugin_settings.set("response_view", "tab")
         utils.set_text_on_view(self.view, "https://httpbin.org/get")
         self.window.run_command("rest_request")
         yield TIMEOUT
@@ -58,6 +73,7 @@ class TestRestClient(DeferrableTestCase):
         self.assertIn("url", body)
 
     def test_get_with_variable(self) -> Iterator[int]:
+        self.plugin_settings.set("response_view", "panel")
         text = "\n".join(
             [
                 "@token = ABC123",
